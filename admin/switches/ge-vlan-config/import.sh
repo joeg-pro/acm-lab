d11f35c7f7607b331321914cab534446cc82585f
Joe Gdaniec <jgdaniec@redhat.com>
2021-11-23 17:56:29 -0500

    Updates for brining new switch online

--- admin/switches/ge-vlan-config/import.sh
+++ admin/switches/ge-vlan-config/import.sh
@@ -8,13 +8,24 @@ tf="terraform-15"
 # The TF config provides an "import_info" output to let this script import
 # everything the TF defines "automatically".
 
+# For special new-switch setup runs...
+special_import_json="./new-switch-import.json"
+use_special_import_json=0
+if [[ -f "$special_import_json" ]]; then
+   use_special_import_json=1
+fi
+
+tmp_import_json="./import-info.json"
 tmp_import_lines="./import-info.lines"
 
-$tf refresh > /dev/null
-$tf output -json import_info | jq -rc '.[] | "\(.resource) \(.id)"' > $tmp_import_lines
+if [[ $use_special_import_json -ne 1 ]]; then
+   $tf refresh > /dev/null
+   $tf output -json import_info > $tmp_import_json
+else
+   cp $special_import_json $tmp_import_json
+fi
 
-# Special import for new switch based on hand-tailored import list:
-# cat ./new-switch-import.json | jq -rc '.[] | "\(.resource) \(.id)"' > $tmp_import_lines
+cat $tmp_import_json | jq -rc '.[] | "\(.resource) \(.id)"' > $tmp_import_lines
 
 while read import_line; do
    resource=${import_line% *}
@@ -22,5 +33,5 @@ while read import_line; do
    "$tf" "import"  "$resource" "$id"
 done < $tmp_import_lines
 
-rm -f $tmp_import_lines
+rm -f $tmp_import_json $tmp_import_lines
 
