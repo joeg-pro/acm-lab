c54cd49b6a927cd5546e05be127adc9c2b8e966e
Joe Gdaniec <jgdaniec@redhat.com>
2021-11-23 17:53:37 -0500

    Updates to bring 1G Switch 3 online

--- admin/switches/ge-slot-config/main.tf
+++ admin/switches/ge-slot-config/main.tf
@@ -25,6 +25,13 @@ provider junos {
   password  = var.switch_password
 }
 
+provider junos {
+  alias     = "sw_ge_3"
+  ip        = "acm-2300-1g-3.mgmt.acm.lab.eng.rdu2.redhat.com"
+  username  = var.switch_username
+  password  = var.switch_password
+}
+
 
 #=== Configuration Info ===
 
@@ -36,7 +43,10 @@ locals {
     00 = ["fog_01", "fog_02", "fog_03", "fog_04", "fog_05", "fog_06"],
     02 = ["fog_07", "fog_08", "fog_09", "fog_10", "fog_11", "fog_12"],
     04 = ["fog_13", "fog_14", "fog_15", "fog_16", "fog_17", "fog_18"],
-    06 = ["fog_19", "fog_20", "fog_21", "fog_22", "fog_23", "fog_24"]
+    06 = ["fog_19", "fog_20", "fog_21", "fog_22", "fog_23", "fog_24"],
+    49 = ["fog_25", "fog_26", "fog_27", "fog_28", "fog_29", "fog_30",
+          "fog_31", "fog_32", "fog_33", "fog_34", "fog_35", "fog_36",
+          "fog_37", "fog_38", "fog_39", "fog_40", "fog_41", "fog_42"]
   }
 
   unallocated_machines = ""
@@ -77,11 +87,33 @@ locals {
     fog_24 = {name="Fog24", nics=[1, 2], ports=[10, 11]}
   }
 
+  sw_ge_3_machine_connections = {
+    fog_25 = {name="Fog25", nics=[1, 2], ports=[0, 1]},
+    fog_26 = {name="Fog26", nics=[1, 2], ports=[2, 3]},
+    fog_27 = {name="Fog27", nics=[1, 2], ports=[4, 5]},
+    fog_28 = {name="Fog28", nics=[1, 2], ports=[6, 7]},
+    fog_29 = {name="Fog29", nics=[1, 2], ports=[8, 9]},
+    fog_30 = {name="Fog30", nics=[1, 2], ports=[10, 11]},
+    fog_31 = {name="Fog31", nics=[1, 2], ports=[12, 13]},
+    fog_32 = {name="Fog32", nics=[1, 2], ports=[14, 15]},
+    fog_33 = {name="Fog33", nics=[1, 2], ports=[16, 17]},
+    fog_34 = {name="Fog34", nics=[1, 2], ports=[18, 19]},
+    fog_35 = {name="Fog35", nics=[1, 2], ports=[20, 21]},
+    fog_36 = {name="Fog36", nics=[1, 2], ports=[22, 23]},
+    fog_37 = {name="Fog37", nics=[1, 2], ports=[24, 25]},
+    fog_38 = {name="Fog38", nics=[1, 2], ports=[26, 27]},
+    fog_39 = {name="Fog39", nics=[1, 2], ports=[28, 29]},
+    fog_40 = {name="Fog40", nics=[1, 2], ports=[30, 31]},
+    fog_41 = {name="Fog41", nics=[1, 2], ports=[32, 33]},
+    fog_42 = {name="Fog42", nics=[1, 2], ports=[34, 35]}
+  }
+
   # Note: Add map entries to local.machine_connections for any new switches.
 
   machine_connections = {
     sw_ge_1 = local.sw_ge_1_machine_connections
     sw_ge_2 = local.sw_ge_2_machine_connections
+    sw_ge_3 = local.sw_ge_3_machine_connections
   }
 
 }
@@ -100,19 +132,23 @@ locals {
 
   # (We do this stepwide to assist in debugging/understanding, maybe.)
 
-  # First, use the slot assinment info to produce an augmented machine connection
-  # map that adds vlan info into the name, port etc. info in the base map.
+  # First, use the slot assinment info to produce an augmented machine connection map
+  # ("machine connections extended") that adds vlan info into the name, port etc. info
+  # defined in the base map.
 
   machine_to_slot_nr_map = transpose(local.machine_slot_assignments)
   machine_connections_ext = {
+    # (Iteration vars: sw_n = switch name, sw_mc = sw_n's machine connections)
     for sw_n,sw_mc in local.machine_connections: sw_n => {
-      for mn,mv in local.machine_connections[sw_n]: mn => {
+      # (Iteration vars: mn = machine key/id, mv = info about machine mn)
+      for mk,mv in local.machine_connections[sw_n]: mk => {
         name  = mv.name, nics  = mv.nics, ports = mv.ports
         vlans = [
           for i in range(length(mv.ports)):
+            # Map to slot's provisioning/data VLAN name based on what NIC it is:
             mv.nics[i] == 1 ?
-              format("test-slot-%02d-prov", tonumber(local.machine_to_slot_nr_map[mn][0])) :
-              format("test-slot-%02d-data", tonumber(local.machine_to_slot_nr_map[mn][0]))
+              format("test-slot-%02d-prov", tonumber(local.machine_to_slot_nr_map[mk][0])) :
+              format("test-slot-%02d-data", tonumber(local.machine_to_slot_nr_map[mk][0]))
         ]
       }
     }
@@ -157,7 +193,7 @@ locals {
   # Collect up import_info for use by import.sh
   #----------------------------------------------
 
-  # Contributes to the import_info output by import.sh:
+  # Contributes to the import_info output for use by import.sh:
   switch_port_import_info = flatten([
     for sw_n,sw_pcm in local.switch_port_configs: [
       for pn,pc in sw_pcm: {
@@ -173,8 +209,6 @@ locals {
 
 # ======== Switch 1 ========
 
-# Ports (Interfaces):
-
 resource junos_interface_physical sw_ge_1_port {
 
   provider = junos.sw_ge_1
@@ -188,8 +222,6 @@ resource junos_interface_physical sw_ge_1_port {
 
 # ======== Switch 2 ========
 
-# Ports (Interfaces):
-
 resource junos_interface_physical sw_ge_2_port {
 
   provider = junos.sw_ge_2
@@ -201,4 +233,16 @@ resource junos_interface_physical sw_ge_2_port {
     vlan_members = each.value.vlans
 }
 
+# ======== Switch 3 ========
+
+resource junos_interface_physical sw_ge_3_port {
+
+  provider = junos.sw_ge_3
+
+  for_each = local.switch_port_configs["sw_ge_3"]
+    name         = each.key
+    description  = each.value.description
+    trunk        = length(each.value.vlans) > 1
+    vlan_members = each.value.vlans
+}
 
