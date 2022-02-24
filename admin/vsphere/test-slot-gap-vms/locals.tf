dc7577f544c1a782144e01e65ffa826bf1dd3037
Joe Gdaniec <jgdaniec@redhat.com>
2022-02-24 17:18:31 -0500

    Move admin tools to acm-lab-admin repo

--- admin/vsphere/test-slot-gap-vms/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  vcenter_username = var.vcenter_user     == "" ? chomp(file(var.vcenter_user_file))     : var.vcenter_user
-  vcenter_password = var.vcenter_password == "" ? chomp(file(var.vcenter_password_file)) : var.vcenter_password
-}
- 
